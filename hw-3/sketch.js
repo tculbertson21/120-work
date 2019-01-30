beb664348cee6d099adb2e1d3acc0107593c2951
Talia Culbertson <46632946+tculbertson21@users.noreply.github.com>
2019-01-30 15:52:35 -0700

    hw-3

---
+++ hw-3/sketch.js
@@ -0,0 +1,13 @@
+//Declare the setup function
+function setup(){
+  //Create a Canvas 600px wide and 400px high
+  createCanvas(600, 400);
+  //color the background 'pink'
+  background('red');
+}
+// Declare the draw function
+function draw(){
+  // Tell p5 to print the text string "Hello World!"
+  // This is going to be positioned at X: 20, y:30
+  text("Hello World!", 20, 30);
+}
